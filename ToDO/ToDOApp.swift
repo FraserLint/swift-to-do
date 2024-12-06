92641d77ad39d3b4ae7f9f7c96d3d2b69553bbd8
FraserLint <davisonaustin287@gmail.com>
2024-12-06 20:22:10 +0000

    Initial Commit

---
+++ ToDO/ToDOApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDOApp.swift
+//  ToDO
+//
+//  Created by Austin Davison on 06/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDOTests/ToDOTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDOTests.swift
+//  ToDOTests
+//
+//  Created by Austin Davison on 06/12/2024.
+//
+
+import Testing
+@testable import ToDO
+
+struct ToDOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
